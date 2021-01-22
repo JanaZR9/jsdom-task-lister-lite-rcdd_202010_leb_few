687f429909d92dbde90e2f0eb97ca8f7e1fe9b0c
Jana Zrayka <janjoune.97@hotmail.com>
2021-01-22 13:13:57 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ let task = document.queryselector("#new-task-description");
 
 form.addEventListener("submit",function(e) {
   e.preventdefault();
+<<<<<<< HEAD
   list.innerHTML = `<li> ${task.value} <li>`
+=======
+  list.innerHTML = `<li>${task.value}<li>`
+>>>>>>> bd64bb22e251e180fe67e138e9055cc5dca20918
 })
 });
